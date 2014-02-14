d15f4f392e235de65c89f94ea4c779141d892060
mschuetz <markus_schuetz@gmx.at>
2014-02-14 19:48:08 +0100

    added Potree.js to gulpfile scripts

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ var paths = {
 		"src/loader/ProceduralPointcloudGenerator.js",
 		"src/loader/PlyLoader.js",
 		"src/utils/LRU.js",
+		"src/Potree.js"
 	],
 	styles: "resources/css/*.css",
 	docs:   "docs/*.md"

12d88685323281d94f80b1f530085fda2f730669
mschuetz <markus_schuetz@gmx.at>
2014-04-27 22:04:06 +0200

    removing unnecessary code

--- src/Potree.js
+++ src/Potree.js
@@ -3,23 +3,3 @@
 function Potree(){
 	
 }
-
-//================================
-// POINT CLOUD
-//================================
-Potree.PointCloud = function(geometry, material){
-	THREE.Object3D.call( this );
-
-	this.geometry = geometry !== undefined ? geometry : new THREE.Geometry();
-	this.material = material !== undefined ? material : new THREE.ParticleSystemMaterial( { color: Math.random() * 0xffffff } );
-}
-
-Potree.PointCloud.prototype = Object.create(THREE.Object3D.prototype);
-
-Potree.PointCloud.prototype.clone = function ( object ) {
-	if (object === undefined) object = new Potree.PointCloud(this.geometry, this.material);
-
-	THREE.Object3D.prototype.clone.call(this, object);
-
-	return object;
-};

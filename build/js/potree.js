54ff40fbf836f0b2d5803766ab805f3095d05306
mschuetz <markus_schuetz@gmx.at>
2015-05-23 17:54:25 +0200

    fix picking bug with point clouds without normals

--- build/js/potree.js
+++ build/js/potree.js
@@ -4739,14 +4739,18 @@ Potree.PointCloudOctree.prototype.pick = function(renderer, camera, ray, params)
 			_gl.vertexAttribPointer( attributePointer, attributeSize, _gl.UNSIGNED_BYTE, true, 0, 0 ); 
 		
 			_gl.uniform1f(material.program.uniforms.pcIndex, material.pcIndex);
+			
+			// TODO: another ugly hack...disable normal attributes, if they're activated
+			var alNormal = _gl.getAttribLocation(program, "normal");
+			if(alNormal >= 0){
+				_gl.disableVertexAttribArray( alNormal );
+			}
+			
 		}	
 		
 		renderer.renderBufferDirect(camera, [], null, material, geometry, object);
 	}
 	
-	
-	
-	var pickWindowSize = 17;
 	var pixelCount = pickWindowSize * pickWindowSize;
 	var buffer = new ArrayBuffer(pixelCount*4);
 	var pixels = new Uint8Array(buffer);

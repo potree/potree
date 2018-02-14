497f6fc576e5e453cb206c66539b295ed7afb18a
m-schuetz <markus_schuetz@gmx.at>
2018-02-14 15:35:05 +0100

    elevation stuff & gradient icons

--- src/materials/shaders/edl.fs
+++ src/materials/shaders/edl.fs
@@ -23,6 +23,7 @@ float response(float depth){
 		vec2 uvNeighbor = vUv + uvRadius * neighbours[i];
 		
 		float neighbourDepth = texture2D(colorMap, uvNeighbor).a;
+		neighbourDepth = (neighbourDepth == 1.0) ? 0.0 : neighbourDepth;
 
 		if(neighbourDepth != 0.0){
 			if(depth == 0.0){
@@ -40,6 +41,7 @@ void main(){
 	vec4 color = texture2D(colorMap, vUv);
 	
 	float depth = color.a;
+	depth = (depth == 1.0) ? 0.0 : depth;
 	float res = response(depth);
 	float shade = exp(-res * 300.0 * edlStrength);
 	
@@ -47,7 +49,6 @@ void main(){
 		discard;
 	}else{
 		gl_FragColor = vec4(color.rgb * shade, opacity);
-		//gl_FragColor = vec4(color.rgb * res, opacity);
 	}
 	
 }

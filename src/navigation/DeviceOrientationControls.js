bd9088ef23a349600c3d3075f413b23f0b9155ce
Chris Lucas <chris.lucas@GEODAN.NL>
2018-03-22 14:53:26 +0100

    spaces to tabs

--- src/navigation/DeviceOrientationControls.js
+++ src/navigation/DeviceOrientationControls.js
@@ -15,63 +15,63 @@
  */
 
 Potree.DeviceOrientationControls = class DeviceOrientationControls extends THREE.EventDispatcher{
-    constructor(viewer){
-        super();
+	constructor(viewer){
+		super();
 
-        this.viewer = viewer;
-        this.renderer = viewer.renderer;
+		this.viewer = viewer;
+		this.renderer = viewer.renderer;
 
-        this.scene = null;
-        this.sceneControls = new THREE.Scene();
+		this.scene = null;
+		this.sceneControls = new THREE.Scene();
 
-        this.screenOrientation = 0;
+		this.screenOrientation = 0;
 
-        let deviceOrientationChange = e => {
-            this.deviceOrientation = e;
-        };
+		let deviceOrientationChange = e => {
+			this.deviceOrientation = e;
+		};
 
-        let screenOrientationChange = e => {
-            this.screenOrientation = window.orientation || 0;
-        };
+		let screenOrientationChange = e => {
+			this.screenOrientation = window.orientation || 0;
+		};
 
-        if ('ondeviceorientationabsolute' in window) {
-            window.addEventListener('deviceorientationabsolute', deviceOrientationChange);
-        } else if ('ondeviceorientation' in window) {
-            window.addEventListener('deviceorientation', deviceOrientationChange);
-        } else {
-            console.warn("No device orientation found.");
-        }
-        // window.addEventListener('deviceorientation', deviceOrientationChange);
-        window.addEventListener('orientationchange', screenOrientationChange);
-    }
+		if ('ondeviceorientationabsolute' in window) {
+			window.addEventListener('deviceorientationabsolute', deviceOrientationChange);
+		} else if ('ondeviceorientation' in window) {
+			window.addEventListener('deviceorientation', deviceOrientationChange);
+		} else {
+			console.warn("No device orientation found.");
+		}
+		// window.addEventListener('deviceorientation', deviceOrientationChange);
+		window.addEventListener('orientationchange', screenOrientationChange);
+	}
 
-    setScene (scene) {
-        this.scene = scene;
-    }
+	setScene (scene) {
+		this.scene = scene;
+	}
 
-    update (delta) {
-        let computeQuaternion = function (alpha, beta, gamma, orient) {
-            let quaternion = new THREE.Quaternion();
+	update (delta) {
+		let computeQuaternion = function (alpha, beta, gamma, orient) {
+			let quaternion = new THREE.Quaternion();
 
-            let zee = new THREE.Vector3(0, 0, 1);
-            let euler = new THREE.Euler();
-            let q0 = new THREE.Quaternion();
+			let zee = new THREE.Vector3(0, 0, 1);
+			let euler = new THREE.Euler();
+			let q0 = new THREE.Quaternion();
 
-            euler.set(beta, gamma, alpha, 'ZXY');
-            quaternion.setFromEuler(euler);
-            quaternion.multiply(q0.setFromAxisAngle(zee, -orient));
+			euler.set(beta, gamma, alpha, 'ZXY');
+			quaternion.setFromEuler(euler);
+			quaternion.multiply(q0.setFromAxisAngle(zee, -orient));
 
-            return quaternion;
-        };
+			return quaternion;
+		};
 
-        if (typeof this.deviceOrientation !== 'undefined') {
-            let alpha = this.deviceOrientation.alpha ? THREE.Math.degToRad(this.deviceOrientation.alpha) : 0;
-            let beta = this.deviceOrientation.beta ? THREE.Math.degToRad(this.deviceOrientation.beta) : 0;
-            let gamma = this.deviceOrientation.gamma ? THREE.Math.degToRad(this.deviceOrientation.gamma) : 0;
-            let orient = this.screenOrientation ? THREE.Math.degToRad(this.screenOrientation) : 0;
+		if (typeof this.deviceOrientation !== 'undefined') {
+			let alpha = this.deviceOrientation.alpha ? THREE.Math.degToRad(this.deviceOrientation.alpha) : 0;
+			let beta = this.deviceOrientation.beta ? THREE.Math.degToRad(this.deviceOrientation.beta) : 0;
+			let gamma = this.deviceOrientation.gamma ? THREE.Math.degToRad(this.deviceOrientation.gamma) : 0;
+			let orient = this.screenOrientation ? THREE.Math.degToRad(this.screenOrientation) : 0;
 
-            let quaternion = computeQuaternion(alpha, beta, gamma, orient);
-            viewer.scene.cameraP.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
-        }
-    }
+			let quaternion = computeQuaternion(alpha, beta, gamma, orient);
+			viewer.scene.cameraP.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
+		}
+	}
 };

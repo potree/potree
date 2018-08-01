db274c4c9b7a0f8eea5156c30fa861bc9557b657
Vinay <vrajur@nextdroidsystems.com>
2018-08-01 14:30:45 -0400

    initial demo created

---
+++ pointclouds/basemap-time/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 106296,
+    "boundingBox": {
+        "lx": 300050.536999,
+        "ly": 4701187.88132,
+        "lz": 240.651457022,
+        "ux": 300226.76310900019,
+        "uy": 4701364.10743,
+        "uz": 416.8775670221633
+    },
+    "tightBoundingBox": {
+        "lx": 300050.536999,
+        "ly": 4701187.88132,
+        "lz": 240.651457022,
+        "ux": 300192.14506,
+        "uy": 4701364.10743,
+        "uz": 246.333015834
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "GPS_TIME"
+    ],
+    "spacing": 1.52616286277771,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

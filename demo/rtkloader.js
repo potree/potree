42c371bd03535e375a37a639a652985aeda5f8ef
Vinay <vrajur@nextdroidsystems.com>
2018-08-08 19:26:53 -0400

    radar and rtk trajectory fully functional

---
+++ demo/rtkloader.js
@@ -0,0 +1,77 @@
+
+function loadRtk(callback) {
+
+  let filename = "csv/rtkdata.csv";
+  let tcol = 1;
+  let xcol = 12;
+  let ycol = 13;
+  let zcol = 14;
+
+  let t0, t1;
+  let tstart = performance.now();
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", filename);
+
+  xhr.onprogress = function(event) {
+    t1 = performance.now();
+    console.log("Loaded ["+event.loaded+"] bytes in ["+(t1-t0)+"] ms")
+    t0 = t1;
+  }
+
+  xhr.onload = function(data) {
+    var t0_loop = performance.now();
+    var rows = data.target.response.split('\n');
+
+    var geometry = new THREE.BufferGeometry();
+    var mpos = [];
+    var positions = [];
+    var timestamps = [];
+    var colors = [];
+
+    let row, cols;
+    for (let ii = 0, len = rows.length; ii < len-1; ++ii) {
+      row = rows[ii];
+      cols = row.split(' ');
+      if (cols.length > 0) {
+        t = parseFloat(cols[tcol]);
+        x = parseFloat(cols[xcol]);
+        y = parseFloat(cols[ycol]);
+        z = parseFloat(cols[zcol]);
+
+
+        if (isNan(t) || isNan(x) || isNan(y) || isNan(z)) {
+          // skip
+          continue;
+        }
+        timestamps.push(t);
+        positions.push(x);
+        positions.push(y);
+        positions.push(z);
+        colors.push( Math.random() * 0xffffff );
+        colors.push( Math.random() * 0xffffff );
+        colors.push( Math.random() * 0xffffff );
+        mpos.push([x,y,z]);
+      }
+    }
+
+    if ( positions.length > 0 ) geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+    if (timestamps.length > 0) geometry.addAttribute('gpsTime', new THREE.Float32BufferAttribute(timestamps, 1));
+    if (colors.length > 0) geometry.addAttribute('color', new THREE.Uint8BufferAttribute(colors, 3));
+    var material = new THREE.PointsMaterial( {size:0.2} );
+    var mesh = new THREE.Points(geometry, material);
+
+    console.log(timestamps);
+    console.log(positions);
+    console.log("Loop Runtime: "+(performance.now()-t0_loop)+"ms");
+    console.log("Full Runtime: "+(performance.now()-tstart)+"ms");
+
+    callback(mpos);
+  };
+
+  t0 = performance.now();
+  xhr.send();
+
+
+
+}

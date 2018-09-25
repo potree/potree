6907659f6ab4aa4ead11a947392d7b5c1d1be510
Bob Cassels <bcassels@nextdroid.com>
2018-09-25 16:26:00 -0400

    Add a postMessage handler, to allow an outer window (if present) to pause the animation. Move all playbar handlers from radar.hyml to playbar.js. (And move them all to jQuery.) Clean up play / pause toggle handling. Remove a few console.log messages.
    Enable loading pointcloud from S3.

--- demo/radarloader.js
+++ demo/radarloader.js
@@ -4,88 +4,91 @@ isNan = function(n) {
   return n !== n;
 }
 
-function loadRadar(callback) {
-
-  let filename = "csv/radar_tracks_demo.csv";
-  let tcol = 3;
-  let xcol = 27;
-  let ycol = 28;
-  let zcol = 29;
-
-
-  let t0, t1;
-  let tstart = performance.now();
-
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", filename);
-  xhr.onprogress = function(event) {
-    t1 = performance.now();
-    console.log("Loaded ["+event.loaded+"] bytes in ["+(t1-t0)+"] ms")
-    t0 = t1;
+function loadRadar(s3, bucket, name, callback) {
+  if (s3 && bucket && name) {
+    const objectName = `${name}/V001/0_Preprocessed/radardata.csv`;
+    s3.getObject({Bucket: bucket,
+                  Key: objectName},
+                 (err, data) => {
+                   if (err) {
+                     console.log(err, err.stack);
+                   } else {
+                     const string = new TextDecoder().decode(data.Body);
+                     const {geometry, t_init} = parseRadar(string);
+                     callback(geometry, t_init);
+                   }});
+  } else {
+    const filename = "csv/radar_tracks_demo.csv";
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", filename);
+    xhr.onload = function(data) {
+      const {geometry, t_init} = parseRadar(data.target.response);
+      callback(geometry, t_init);
+    };
+    xhr.send();
   }
-  xhr.onload = function(data) {
-    var t0_loop = performance.now();
-    var rows = data.target.response.split('\n');
+}
 
-    var geometry = new THREE.BufferGeometry();
-    var positions = [];
-    var timestamps = [];
-    var colors = [];
-    var alphas = [];
+function parseRadar(radarString) {
+  const t0_loop = performance.now();
+  const rows = radarString.split('\n');
 
-    let row, cols;
-    let t_init = 0;
-    let firstTimestamp = true;
-    for (let ii = 0, len = rows.length; ii < len-1; ++ii) {
-      row = rows[ii];
-      cols = row.split(',');
+  const tcol = 3;
+  const xcol = 27;
+  const ycol = 28;
+  const zcol = 29;
 
-      if (cols.length > 0) {
-        t = parseFloat(cols[tcol]);
-        x = parseFloat(cols[xcol]);
-        y = parseFloat(cols[ycol]);
-        z = parseFloat(cols[zcol]);
+  const geometry = new THREE.BufferGeometry();
+  let positions = [];
+  let timestamps = [];
+  let colors = [];
+  let alphas = [];
 
+  let t_init = 0;
+  let firstTimestamp = true;
+  for (let ii = 0, len = rows.length; ii < len-1; ++ii) {
+    const row = rows[ii];
+    const cols = row.split(',');
 
-        if (isNan(t) || isNan(x) || isNan(y) || isNan(z)) {
-          // skip
-          continue;
-        }
+    if (cols.length > 0) {
+      const t = parseFloat(cols[tcol]);
+      const x = parseFloat(cols[xcol]);
+      const y = parseFloat(cols[ycol]);
+      const z = parseFloat(cols[zcol]);
 
-
-        if (firstTimestamp) {
-          t_init = t;
-          firstTimestamp = false;
-        }
-
-        // timestamps.push(t);
-        timestamps.push(t-t_init);
-        positions.push(x);
-        positions.push(y);
-        positions.push(z);
-        colors.push( Math.random() * 0xffffff );
-        colors.push( Math.random() * 0xffffff );
-        colors.push( Math.random() * 0xffffff );
-        alphas.push( 1.0 );
+      if (isNan(t) || isNan(x) || isNan(y) || isNan(z)) {
+        // skip
+        continue;
       }
+
+      if (firstTimestamp) {
+        t_init = t;
+        firstTimestamp = false;
+      }
+
+      // timestamps.push(t);
+      timestamps.push(t-t_init);
+      positions.push(x);
+      positions.push(y);
+      positions.push(z);
+      colors.push( Math.random() * 0xffffff );
+      colors.push( Math.random() * 0xffffff );
+      colors.push( Math.random() * 0xffffff );
+      alphas.push( 1.0 );
     }
-    // debugger; // timestamp
+  }
+  // debugger; // timestamp
 
 
-    if ( positions.length > 0 ) geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
-    if (timestamps.length > 0) geometry.addAttribute('gpsTime', new THREE.Float32BufferAttribute(timestamps, 1));
-    if (colors.length > 0) geometry.addAttribute('color', new THREE.Uint8BufferAttribute(colors, 3));
-    if (alphas.length > 0) geometry.addAttribute('alpha', new THREE.Float32BufferAttribute(alphas, 1));
+  if ( positions.length > 0 ) geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+  if (timestamps.length > 0) geometry.addAttribute('gpsTime', new THREE.Float32BufferAttribute(timestamps, 1));
+  if (colors.length > 0) geometry.addAttribute('color', new THREE.Uint8BufferAttribute(colors, 3));
+  if (alphas.length > 0) geometry.addAttribute('alpha', new THREE.Float32BufferAttribute(alphas, 1));
 
-    console.log("timestamps");
-    console.log(timestamps);
-    console.log(positions);
-    console.log("Loop Runtime: "+(performance.now()-t0_loop)+"ms");
-    console.log("Full Runtime: "+(performance.now()-tstart)+"ms");
+  // console.log("timestamps");
+  // console.log(timestamps);
+  // console.log(positions);
+  console.log("Loop Runtime: "+(performance.now()-t0_loop)+"ms");
 
-    callback(geometry, t_init);
-  };
-
-  t0 = performance.now();
-  xhr.send();
+  return {geometry, t_init};
 }

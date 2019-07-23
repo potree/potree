84d75ed8c6e79f880b9d738e6c193232ec6769d5
m-schuetz <mschuetz@potree.org>
2019-07-23 16:38:36 +0200

    initial VR test

--- src/PotreeRenderer.js
+++ src/PotreeRenderer.js
@@ -661,6 +661,8 @@ export class Renderer {
 
 	renderNodes(octree, nodes, visibilityTextureData, camera, target, shader, params) {
 
+		//console.log(`renderNodes: ${nodes.length}`);
+
 		if (exports.measureTimings) performance.mark("renderNodes-start");
 
 		let gl = this.gl;

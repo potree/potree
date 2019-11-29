eb8213d70e931c6f4524af19f29f02152991820d
Tiago Sá <tiagodts@gmai.com>
2019-11-29 14:40:52 -0300

    pt-BR Translation
    Thanks to raysonn.

--- src/viewer/viewer.js
+++ src/viewer/viewer.js
@@ -1135,7 +1135,7 @@ export class Viewer extends EventDispatcher{
 			i18n.init({
 				lng: 'en',
 				resGetPath: Potree.resourcePath + '/lang/__lng__/__ns__.json',
-				preload: ['en', 'fr', 'de', 'jp', 'se'],
+				preload: ['en', 'fr', 'de', 'jp', 'se', 'pt-BR'],
 				getAsync: true,
 				debug: false
 			}, function (t) {

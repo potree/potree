521b9d5a90781a59cdc0143eb526ed1df3ce2bac
Rune <rune@addin.dk>
2020-03-19 07:30:09 +0100

    Update POCLoader.js
    CLASSIFICATION

--- src/loader/POCLoader.js
+++ src/loader/POCLoader.js
@@ -15,7 +15,8 @@ function parseAttributes(cloudjs){
 	const replacements = {
 		"COLOR_PACKED": "rgba",
 		"RGBA": "rgba",
-		"INTENSITY": "intensity"
+		"INTENSITY": "intensity",
+		"CLASSIFICATION": "classification"
 	};
 
 	const replaceOldNames = (old) => {

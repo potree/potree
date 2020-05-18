2ba2354d624089e0ae498c36aec09ae5b801dd77
Roy Arents <roy.arents@nlr.nl>
2020-05-18 17:44:21 +0200

    Updating Cesium (step 2-2)

---
+++ libs/Cesium/Workers/GeometryAttributes-fbf888b4.js
@@ -0,0 +1,1 @@
+define(["exports","./when-b43ff45e"],function(t,n){"use strict";t.GeometryAttributes=function(t){t=n.defaultValue(t,n.defaultValue.EMPTY_OBJECT),this.position=t.position,this.normal=t.normal,this.st=t.st,this.bitangent=t.bitangent,this.tangent=t.tangent,this.color=t.color}});

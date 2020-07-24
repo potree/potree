c2b68c38c7552d53c2c3294bf8766eafea8b4f39
Nate Haddad <nhaddad@nextdroid.com>
2020-07-24 17:03:15 -0400

    Initial commit: includes several new functions for uploading annotated lanes flatbuffer files to s3. Functions incomplete and still need to be integrated with the existing code

--- common/playbar.js
+++ common/playbar.js
@@ -402,3 +402,273 @@ function addPlaybarListeners() {
 		viewer.setFilterGPSTimeExtent(gpsTime + 2.5 * timeMin, gpsTime + 2.5 * timeMax);
 	});
 }
+
+
+function updateLane () {
+  // create lanes flatbuffer
+  const lane = {
+    id: 0,
+    timestamp: [],
+    left: [],
+    right: [],
+    laneTypeLeft: [],
+    laneTypeRight: [],
+    leftPointValidity: [],
+    rightPointValidity: [],
+    leftPointAnnotationStatus: [],
+    rightPointAnnotationStatus: []
+  }
+
+  // Left Lane Vertices:
+  const laneLeftSegments = window.viewer.scene.scene.getChildByName("Left Lane Segments");
+  if (laneLeftSegments === undefined) {
+    const laneLeft = window.viewer.scene.scene.getChildByName("Lane Left");
+    lane.left = laneLeft.points; // download(JSON.stringify(laneLeft.points, null, 2), "lane-left.json");
+  } else {
+    lane.left = laneLeftSegments.getFinalPoints(); // download(JSON.stringify(laneLeftSegments.getFinalPoints(), null, 2), "lane-left.json");
+  }
+
+  // Right Lane Vertices:
+  const laneRightSegments = window.viewer.scene.scene.getChildByName("Right Lane Segments");
+  if (laneRightSegments === undefined) {
+    const laneRight = window.viewer.scene.scene.getChildByName("Lane Right");
+    lane.right = laneRight.points; // download(JSON.stringify(laneRight.points, null, 2), "lane-right.json", "text/plain");
+  } else {
+    lane.right = laneRightSegments.getFinalPoints(); // download(JSON.stringify(laneRightSegments.getFinalPoints(), null, 2), "lane-right.json", "text/plain");
+  }
+
+  // Get New Spine Vertices
+  lane.spine = updateSpine(lane.left, lane.right);
+  // Update Lane attributes to be valid
+  updateLaneHelper(lane);
+
+  const bytes = createLanesFlatbuffer([lane]);
+
+  writeFileToS3(s3, bucket, name, "2_Truth", "upload-testing-lanes.fb", bytes);
+}
+
+export async function writeFileToS3 (s3, bucket, name, subdirectory, filename, buffer) {
+  try {
+    const request = s3.putObject({
+      Bucket: bucket,
+      Key: `${name}/${subdirectory}/${filename}`,
+      Body: buffer
+    });
+    request.on("httpUploadProgress", async (e) => {
+      await updateLoadingBar(e.loaded / e.total * 100)
+    });
+    await request.promise();
+    incrementLoadingBarTotal("lanes uploaded")
+  } catch (e) {
+    console.error("Error: could not write file to S3: ", e);
+  }
+}
+
+function updateLaneHelper (lane) {
+  const spineLength = lane.spine.length;
+  const leftLength = lane.left.length;
+  const rightLength = lane.right.length;
+  lane.timestamp = Array.from({length: spineLength}).map(x => 0.0)
+  lane.leftPointValidity = Array.from({length: leftLength}).map(x => Flatbuffer.GroundTruth.PointValidity(0));
+  lane.rightPointValidity = Array.from({length: rightLength}).map(x => Flatbuffer.GroundTruth.PointValidity(0));
+  lane.leftPointAnnotationStatus = Array.from({ length: leftLength }).map(x => Flatbuffer.GroundTruth.PointAnnotationStatus(0));
+  lane.rightPointAnnotationStatus = Array.from({ length: rightLength }).map(x => Flatbuffer.GroundTruth.PointAnnotationStatus(0));
+}
+
+export function createLanesFlatbuffer (input) {
+  const lanesArray = []
+  const builder = new flatbuffers.flatbuffers.Builder(0);
+  // create each lane and push to lanesArray
+  for (const lane of input.all) {
+    Flatbuffer.GroundTruth.Lane.startLane(builder);
+    Flatbuffer.GroundTruth.Lane.addId(builder, lane.id);
+    Flatbuffer.GroundTruth.Lane.addTimestamp(builder, lane.timestamp);
+    Flatbuffer.GroundTruth.Lane.addLeft(builder, lane.left);
+    Flatbuffer.GroundTruth.Lane.addRight(builder, lane.right);
+    Flatbuffer.GroundTruth.Lane.addlaneTypeLeft(builder, lane.laneTypeLeft);
+    Flatbuffer.GroundTruth.Lane.addLaneTypeRight(builder, lane.laneTypeRight);
+    Flatbuffer.GroundTruth.Lane.addLeftPointValidity(builder, lane.leftPointValidity);
+    Flatbuffer.GroundTruth.Lane.addRightPointValidity(builder, lane.rightPointValidity);
+    Flatbuffer.GroundTruth.Lane.addLeftPointAnnotationStatus(builder, lane.leftPointAnnotationStatus);
+    Flatbuffer.GroundTruth.Lane.addRightPointAnnotationStatus(builder, lane.rightPointAnnotationStatus);
+    const newLane = Flatbuffer.GroundTruth.Lane.endLane(builder);
+    lanesArray.push(newLane)
+  }
+  const lanes = Flatbuffer.GroundTruth.Lanes.createLanesVector(builder, lanesArray);
+  Flatbuffer.GroundTruth.Lanes.finishLanesBuffer(builder, lanes);
+  const bytes = builder.asUint8Array();
+  const lenBytes = new Uint8Array(4);
+  const dv = new DataView(lenBytes.buffer);
+  dv.setUint32(0, bytes.length, true);
+
+  const outbytes = new Int8Array(lenBytes.length + bytes.length);
+  outbytes.set(lenBytes);
+  outbytes.set(bytes, lenBytes.length);
+  return outbytes;
+}
+
+
+function getXYZ(data) {
+    x = data['position']['x'];
+    y = data['position']['y'];
+    z = data['position']['z'];
+    return [x, y, z];
+}
+
+function getHeading(pt1, pt2) {
+    p1 = np.array(pt1).transpose()
+    p2 = np.array(pt2).transpose()
+    delta = p2-p1;
+    heading = Math.atan2(delta[1], delta[0]);
+    return heading;
+}
+
+function minAngle(theta, bounds=[-Math.pi, Math.pi]) {
+    while (theta < bounds[0]) {
+        theta += 2 * Math.pi
+    }
+    while (theta > bounds[1]) {
+        theta -= 2 * Math.pi
+    }
+    return theta;
+}
+
+function minAngleDiff(theta1, theta2, bounds=[-np.pi, np.pi]) {
+    delta = theta2 - theta1;
+    return minAngle(delta);
+}
+
+function normalizeVector(vector) {
+    vector = Array(vector);
+    return vector / math.norm(vector)
+}
+
+
+function find_plane_line_intersection(plane_pt, plane_normal, line_pt, line_direction) {
+    plane_normal = normalizeVector(plane_normal);
+    line_direction = normalizeVector(line_direction);
+
+    alpha = math.dot(plane_normal, plane_pt) - math.dot(plane_normal, line_pt) / math.dot(plane_normal, line_direction)
+    return line_pt + alpha * line_direction;
+}
+
+
+function updateSpine (laneLefts, laneRights) {
+    laneChangeNNRange = 5 // meters
+    rightLaneNeighborPointsQueryNumber = 10 // number of neighbors
+    laneChangeHeadingThresh = np.pi/4 // Radians
+    prevPointSuppressionRadius = .1 // meters
+    // Compute Spine Vertices (using left lane as reference):
+    // PROBLEM: No shapely libary in JS need to find alternative as we need interpolation and projection functions below
+    rightLine = LineString(laneRights) // Create shapely linestring for right line
+    // PROBLEM no built in libraries for this one either. Can build tree structure on our own though
+    kdtree = cKDTree(laneRights) // Create KD-Tree for right lane points as well
+    lastIdx = len(laneLefts)-1
+    for (let ii = 0; ii < lastIdx+1; ii++) {
+        p = laneLefts[ii];
+        pt = Point(p);
+        projPt = rightLine.interpolate(rightLine.project(pt));
+        minLine = LineString([pt, projPt]) // Shortest line from pt to right line
+        spinePt = minLine.interpolate(minLine.length / 2)
+
+        if (ii !== 0 && ii !== lastIdx) {
+            lastPt = Point(laneLefts[ii-1]);
+            nextPt = Point(laneLefts[ii+1]);
+            lastHeading = getHeading(lastPt, pt);
+            nextHeading = getHeading(pt, nextPt);
+
+            leftHeadingDelta = minAngleDiff(lastHeading, nextHeading);
+
+            if (Math.abs(leftHeadingDelta) > laneChangeHeadingThresh) {
+
+                // Get idxs of nearby right points
+                dists_nn, idxs_nn = kdtree.query(p, rightLaneNeighborPointsQueryNumber)
+
+                filterer = dists_nn <= laneChangeNNRange;
+                dists_nn = dists_nn[filterer];
+                idxs_nn = idxs_nn[filterer];
+
+                if (idxs_nn.length === 0) { continue }
+
+                idxs_nn_min = Math.max(idxs_nn.min()-1, 0)
+                idxs_nn_max = Math.min(idxs_nn.max()+2, laneRights.length)
+
+                idxs_nn = np.arange(idxs_nn_min, idxs_nn_max) // Closed set of points approximately within laneChangeNNRange of pt
+
+                if (idxs_nn.length < 3) {
+                    console.log(`Not Enough Neighbors for pt: [idx: ${ii}, pos: ${p}]`);
+                    continue
+                }
+                // Find closest lane change point in right line:
+                rightPts = Array(laneRights)[idxs_nn]
+                rightPtDeltas = np.diff(rightPts, axis=0)
+                rightHeadings = Math.atan2(rightPtDeltas[:,1], rightPtDeltas[:,0])
+                rightHeadingDeltas = Array([minAngle(x) for x in np.diff(rightHeadings)])
+
+
+                if (rightHeadingDeltas.shape[0] == 0) {
+                    console.log(`No RightHeadingDeltas for pt: [idx: ${ii}, pos: ${p}]`);
+                    continue
+                }
+                leftRightHeadingDeltaSimilarity = Math.abs(rightHeadingDeltas-leftHeadingDelta)
+                bestRightHeadingIdx = leftRightHeadingDeltaSimilarity.argmin() + 1
+                rightPtIdx = idxs_nn[bestRightHeadingIdx] // +1 inside []
+
+                // Simple Solution (Just Compute Spine Point using the Right Lane Change Point and append):
+                rightPt = Point(laneRights[rightPtIdx])
+                minLine = LineString([pt, rightPt])
+                spinePt2 = minLine.interpolate(minLine.length / 2)
+
+                p1 = Array(laneSpines[-1].slice(2)])
+                p2 = Array(spinePt2.xy).flatten()
+
+                if (math.norm((p2-p1)) > prevPointSuppressionRadius) { // If spine point is farther than thresh from previous point append
+                    laneSpines.push([spinePt2.x, spinePt2.y, spinePt2.z])
+                }
+            }
+        }
+        // Don't append lane spine if duplicate (within 10cm of last) or if going backwards:
+        function should_append(spinePt) {
+
+            if (laneSpines.length < 1) {
+                return true;
+            }
+
+            // TODO double check with original
+            p1 = Array(laneSpines[-1]).slice(2);
+            p2 = Array(spinePt.xy).flatten()
+
+            if (np.linalg.norm(p2-p1) < prevPointSuppressionRadius) {
+                console.log("Skipping lane spine point-- too close to existing spine point");
+                return false;
+            } else {
+                if (laneSpines.length >=2) {
+                    p0 = Array(laneSpines[-2]).slice(2);
+
+                    v1 = (p1-p0).slice(2);
+                    v2 = (p2-p1).slice(2);
+
+                    magV1 = math.norm(v1)
+                    magV2 = math.norm(v2)
+
+                    cosHeading = math.dot(v1, v2)/(magV1 * magV2)
+
+                    if (Math.abs(cosHeading - (-1)) < .1) {
+                        console.log("Skipping lane spine point -- going backwards");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+        } //end function should_append
+
+        if (should_append(spinePt)) {
+            laneSpines.push([spinePt.x, spinePt.y, spinePt.z])
+        }
+    }
+    // laneSpines = Array(laneSpines[laneSpines.length])
+    return laneSpines
+}

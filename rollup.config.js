ec04a7bf439202b951f9039a59fff2bf28e0f61c
Mauro Porras <mauroporrasc@gmail.com>
2020-09-15 08:32:33 -0500

    build(npm)!: Add UMD support (#7)
    BREAKING CHANGE: Raw imports are no longer supported.
    
    Close #8

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,37 @@
-import webWorkerLoader from 'rollup-plugin-web-worker-loader'
 import { terser } from 'rollup-plugin-terser'
+import webWorkerLoader from 'rollup-plugin-web-worker-loader'
 
 import pkg from './package.json'
 
-// Transforms imports for raw imports.
-// We use this for unit testing, so we can use the libraries
-// without needing a bundling tool like WebPack.
-// Raw imports require relative paths that can be resolved
-// from the location of the current module right to the esm module.
-// So we assume zea-cad is installed
-// node_modules\@zeainc\zea-cad\dist
-// And to get to zea-engine, we need to go up 2 folders and down into
-// the folder for zea-engine and find the esm.js file.
-// Normally WebPack uses the package.json file to resolve the file
-// at compile time, but we want to load modules dynamically
-const plugin = {
-  name: 'transform-imports',
-  renderChunk(code, chunk, options) {
-    return code.replace(
-      '@zeainc/zea-engine',
-      '../../zea-engine/dist/index.esm.js'
-    )
-  }
+const external = ['@zeainc/zea-engine']
+
+const plugins = [webWorkerLoader()]
+
+const isProduction = !process.env.ROLLUP_WATCH
+
+if (isProduction) {
+  plugins.push(terser())
 }
 
+const sourcemap = true
+
 export default [
+  // Browser-friendly UMD build.
+  {
+    input: 'src/zea/index.js',
+    external,
+    output: {
+      name: 'zeaPointClouds',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap,
+      globals: {
+        '@zeainc/zea-engine': 'zeaEngine',
+      },
+    },
+    plugins,
+  },
+
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
@@ -33,12 +40,11 @@ export default [
   // `file` and `format` for each target)
   {
     input: 'src/zea/index.js',
-    external: [...Object.keys(pkg.dependencies)],
-    plugins: [terser(), webWorkerLoader()],
+    external,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-      { file: pkg.rawimport, format: 'es', plugins: [plugin] },
+      { file: pkg.main, format: 'cjs', sourcemap },
+      { file: pkg.module, format: 'es', sourcemap },
     ],
+    plugins,
   },
 ]

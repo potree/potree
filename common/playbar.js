ea66c407a8a172f5b017b7f37bad1fb376161533
jterranovaND <jterranova@nextdroid.com>
2021-02-08 16:15:16 -0500

    revert playbar change

--- common/playbar.js
+++ common/playbar.js
@@ -468,8 +468,7 @@ function callUpdateLanesLambdaFunction (bucket, name, lane) {
   lambda.invoke({
     FunctionName: 'UpdateLanes:$LATEST',
     LogType: 'None',
-    Payload: JSON.stringify(payload),
-
+    Payload: JSON.stringify(payload)
   }, function (err, data) {
     if (err) {
       console.log(err, err.stack);

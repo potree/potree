b2648024cc8bdf2db593872e9f002c021cf83f01
OlivierLeal <olivierleal61@gmail.com>
2024-09-11 10:34:35 -0300

    feat: added portuguese brazilian translation

--- src/viewer/sidebar.js
+++ src/viewer/sidebar.js
@@ -1243,7 +1243,8 @@ export class Sidebar{
 			["SE", "se"],
 			["ZH", "zh"],
 			["IT", "it"],
-			["CA", "ca"]
+			["CA", "ca"],
+			["PT-BR", "pt-BR"]
 		];
 
 		let elLanguages = $('#potree_languages');
